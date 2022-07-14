5a1c9bad529f7b520bd20511d6e8279671f1f56b
rrkincaid <rrkincaid@gmail.com>
2022-07-14 09:35:31 -0600

    pushing to heroku

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,13 +4,14 @@ export default function home() {
   return (
     <div>
       <h2> Bio </h2>
+      {/* <img src="./assets/rkpic.jpg" /> */}
       <h5>
         Hi! My name is Rachel and I am a web developer located in Colorado. I
-        began coding fairly recently but thanks to DU Bootcamp and their amazing
-        staff and my fellow students, I can now count myself as a full stack web
-        developer. I am building my portfolio currently and if you wish to see
-        projects that I have worked on in the past, please follow the links
-        above to explore those. Pretty exciting stuff here!
+        began coding fairly recently but thanks to DU Coding Bootcamp and their
+        amazing staff and my fellow students, I can now count myself as a full
+        stack web developer. Please explore this page to see the projects I have
+        been working on recently and to learn a little more about me. Follow the
+        links above to navigate through the page. Pretty exciting stuff here!
       </h5>
     </div>
   );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,26 +5,38 @@ export default function projects() {
   return (
     <div>
       <h2>Past Projects</h2>
-      <div>
+      <div className="proj1">
         <h5> Everything But the Kitchen Sink </h5>
         <p> Recipe App that uses selected ingredients you have on-hand </p>
       </div>
 
-      <div>
+      <div className="proj2">
         <h5> Crypt Keepers </h5>
         <p> Cryptocurrency Learning Site</p>
       </div>
 
-      <div>
+      <div className="proj3">
         <h5> COMING SOON: Ain't Life Sweet? </h5>
         <p> E-commerce site for selling ice cream </p>
       </div>
 
-      <div>
+      <div className="classproj">
         <h5> In-Class Projects - 1: Weather App </h5>
         <h5> In-Class Projects - 2: Tolkien Quiz </h5>
         <h5> In-Class Projects - 3: Express Note Taker </h5>
       </div>
+
+      {/* <div className="cardcontainer d-flex flex-wrap justify-content-evenly">
+        <div className="card m-5 p-2" style={{ width: "18rem" }}>
+          <img src={process.env.PUBLIC_URL + "rkpic.jpg"} alt="proj1">
+            {" "}
+          </img>
+          <div className="card-body">
+            <h5 className="card-title"> PROJECT NAME </h5>
+            <p className="card-text"></p>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
